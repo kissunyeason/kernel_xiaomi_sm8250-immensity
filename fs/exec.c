bd6276dd5249b85ada5b6caf479e5c74dd269639
kissunyeason <kissuny@live.com>
2023-06-13 22:13:22 +0800

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1913,7 +1913,8 @@ out_ret:
 		putname(filename);
 	return retval;
 }
-
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+void *envp, int *flags);
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
